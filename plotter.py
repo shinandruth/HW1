f08f9eb441f48f09ff28ac33fe4c557a6c738960
Joshua Koo <joshuakoo2019@u.northwestern.edu>
2018-04-18 18:12:18 -0500

    with plot stuff

--- plotter.py
+++ plotter.py
@@ -1,4 +1,6 @@
 import ID3, parse, random
+import matplotlib.pyplot as plt
+
 
 def plotter(inFile):
     pruneAverages = []
@@ -9,6 +11,8 @@ def plotter(inFile):
         trainingSizes.append(x*10)
         pruneAverages.append(mytuple[0])
         nopruneAverages.append(mytuple[1])
+    plt.plot(pruneAverages, trainingSizes, "g^")
+    plt.plot(nopruneAverages, trainingSizes, "bs")
 
 
 
@@ -19,34 +23,34 @@ def plot(inFile, training_size):
     data = parse.parse(inFile)
     for i in range(100):
         random.shuffle(data)
-        train = data[:len(data)*0.7]
-        valid = data[len(data)*0.7:testsize]
+        train = data[:7*len(data)/10]
+        valid = data[7*len(data)/10:testsize]
         test = data[testsize:]
 
         tree = ID3.ID3(train, 'democrat')
         acc = ID3.test(tree, train)
-        print "training accuracy: ",acc
+        #print "training accuracy: ",acc
         acc = ID3.test(tree, valid)
-        print "validation accuracy: ",acc
+        #print "validation accuracy: ",acc
         acc = ID3.test(tree, test)
-        print "test accuracy: ",acc
+        #print "test accuracy: ",acc
 
         ID3.prune(tree, valid)
         acc = ID3.test(tree, train)
-        print "pruned tree train accuracy: ",acc
+        #print "pruned tree train accuracy: ",acc
         acc = ID3.test(tree, valid)
-        print "pruned tree validation accuracy: ",acc
+        #print "pruned tree validation accuracy: ",acc
         acc = ID3.test(tree, test)
-        print "pruned tree test accuracy: ",acc
+        #print "pruned tree test accuracy: ",acc
         withPruning.append(acc)
         tree = ID3.ID3(train+valid, 'democrat')
         acc = ID3.test(tree, test)
-        print "no pruning test accuracy: ",acc
+        #print "no pruning test accuracy: ",acc
         withoutPruning.append(acc)
-    plot(train)
-    print withPruning
-    print withoutPruning
-    print "average with pruning",sum(withPruning)/len(withPruning)," without: ",sum(withoutPruning)/len(withoutPruning)
+    #plot(train)
+    #print withPruning
+    #print withoutPruning
+    #print "average with pruning",sum(withPruning)/len(withPruning)," without: ",sum(withoutPruning)/len(withoutPruning)
     pruneAvg = sum(withPruning)/len(withPruning)
     nopruneAvg = sum(withoutPruning)/len(withoutPruning)
     return (pruneAvg, nopruneAvg)
---
+++ tester.py
@@ -0,0 +1,7 @@
+def main():
+
+    for x in range(10, 300):
+        print x
+
+if __name__ == "__main__":
+    main()
